52b42bd56ad1b42f46dcb02b7fc9b1efd5f1d458
CapDRAKE <72074285+CapDRAKE@users.noreply.github.com>
2023-02-22 16:21:43 +0100

    Add connexion BDD

---
+++ src/main/java/com/example/projet_architecture_logicielle_4al_groupe_9/modele/SQL.java
@@ -0,0 +1,28 @@
+package com.example.projet_architecture_logicielle_4al_groupe_9.modele;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQL {
+    private final String IP = "91.121.230.45";
+    private final int PORT = 3306;
+    private final String USER = "ArchiLogiciel";
+    private final String NOM_BDD = "ArchiLogiciel";
+    private final String MDP = "%FyKr08_*-mkG34P";
+    private Connection connexion;
+
+    public SQL() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connexion = DriverManager.getConnection("jdbc:mysql://" + IP + ":" + PORT + "/" + NOM_BDD, USER, MDP);
+            System.out.println("Connexion à la base de données réussie.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erreur de chargement du driver MySQL.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Erreur de connexion à la base de données.");
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/com/example/projet_architecture_logicielle_4al_groupe_9/ProjetArchitectureLogicielle4alGroupe9ApplicationTests.java
+++ src/test/java/com/example/projet_architecture_logicielle_4al_groupe_9/ProjetArchitectureLogicielle4alGroupe9ApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.projet_architecture_logicielle_4al_groupe_9;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.projet_architecture_logicielle_4al_groupe_9.modele.SQL;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProjetArchitectureLogicielle4alGroupe9ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	/**
+	 * Test de connexion à notre BDD
+	 */
+	void testConnexionSQL() {
+		SQL sql = new SQL();
+		assertNotNull(sql);
 	}
 
 }
